a0ce42e649e0da62e10618901990e5dc85b34f95
mrsteveson <patrick.steveson727@gmail.com>
2019-05-30 14:39:13 -0500

    typo in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const server = require('./server.js');
+const server = require('./API/server.js');
 const port = process.env.PORT || 5000;
 
 server.listen(port, function() {
