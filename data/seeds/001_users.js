c56c0a5e26eddd5bb3d83d6aa474d11bb397b42b
AJLambda <apj2277@gmail.com>
2019-05-31 16:35:49 -0700

    creates helper function to ensure unique vales in seeds

--- data/seeds/001_users.js
+++ data/seeds/001_users.js
@@ -1,12 +1,24 @@
 const faker = require("faker");
 
+const genUniqueArr = (num, fakerFn) => {
+  let arr = [];
+  while (arr.length < num) {
+    const temp = fakerFn();
+    if (!arr.includes(temp)) {
+      arr.push(temp);
+    }
+  }
+  return arr;
+};
+
 const generateSeeds = () => {
   const numOfUsers = 500;
+  const usernameUnique = genUniqueArr(numOfUsers, faker.internet.userName);
   let arr = [];
   for (let i = 0; i < numOfUsers; i++) {
     arr.push({
       //.findName() creates a full name (first and last)
-      username: faker.name.findName(),
+      username: usernameUnique[i],
       password: faker.lorem.word(),
       role: "user"
     });
