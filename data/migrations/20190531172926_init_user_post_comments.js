7a1f652096b4b051054c3e38e56580c5e8001a5d
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-31 17:35:22 -0400

    Initializes user_post_comments table

---
+++ data/migrations/20190531172926_init_user_post_comments.js
@@ -0,0 +1,27 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('user_post_comments', user_post_comments => {
+    user_post_comments.increments();
+
+    user_post_comments.integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+
+    user_post_comments.integer('post_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('posts')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+
+    user_post_comments.text('comment')
+  })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('user_post_comments');
+};
