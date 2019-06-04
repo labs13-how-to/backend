75fdbd69e582677cf2cc09654b9566b43a724b2d
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-06-04 13:40:55 -0400

    Should complete POST for posts

--- API/posts/posts-model.js
+++ API/posts/posts-model.js
@@ -3,31 +3,36 @@ const db = require("../../data/dbConfig.js");
 module.exports = {
     getAllPosts,
     getPostById,
-    addNew,
-    remove,
-    update
+    createPost,
+    removePost,
+    updatePost
 }
 
 function getAllPosts() {
     return db("posts")
 };
 
-async function getPostById(id) {
+function getPostById(id) {
+    // Use promise for router error handling
     return new Promise(async (resolve, reject) => {
         let post, steps;
         try {
+            // Use knex transaction to only call to DB once
             await db.transaction(async trx => {
+                // Grab the specified post
                 post = await db("posts")
                     .where({ id })
                     .first()
                     .returning('*')
                     .transacting(trx);
+                // Grab the steps for that post
                 steps = await db("post_steps")
                     .where({ post_id: id })
+                    .orderBy('step_num')
                     .transacting(trx);
             })
             !post
-                ? resolve(null)
+                ? resolve(null) // If post doesn't exist, return null to trigger 404
                 : resolve({ ...post, steps });
         } catch(err) {
             reject(err);
@@ -35,22 +40,41 @@ async function getPostById(id) {
     })
 };
 
-function addNew(post) {
-    return db("posts")
-    .insert(post)
-    .then(id => {
-        return getPostById(id[0])
-    });
+function createPost(post) {
+    let {steps, ...rest} = post;
+    // rest = {...rest, created_by} // Insert user_id from JWT
+    let post_id;
+    // Use promise for router error handling
+    return new Promise(async (resolve, reject) => {
+        try {
+            await db.transaction(async trx => {
+                // Create new post and grab its ID
+                [post_id] = await db("posts")
+                    .insert(rest)
+                    .returning('id')
+                    .transacting(trx);
+                // Create steps w/ ID of the post they're associated with
+                steps = steps.map(step => ({...step, post_id}))
+                console.log(steps)
+                await db("post_steps")
+                    .insert(steps)
+                    .transacting(trx);
+            })
+            resolve(post_id);
+        } catch(err) {
+            reject(err)
+        }
+    })
 };
 
-function remove(id) {
+function removePost(id) {
     return db("posts")
     .where({ id })
     .first()
     .del()
 };
 
-function update(id, changes) {
+function updatePost(id, changes) {
     return db("posts")
     .where({ id })
     .update(changes)
