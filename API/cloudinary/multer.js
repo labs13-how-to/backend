2484576cb27899b6dc676add76d6ceb4fa07d702
mrsteveson <patrick.steveson727@gmail.com>
2019-06-04 12:41:28 -0500

    fixed an error causing dataUri to be undefined

--- API/cloudinary/multer.js
+++ API/cloudinary/multer.js
@@ -6,9 +6,8 @@ const storage = multer.memoryStorage();
 const multerUploads = multer({ storage }).single("image");
 const dUri = new Datauri();
 
-const dataUri = req => {
+const dataUri = req => 
     dUri.format(path.extname(req.file.originalname).toString(), req.file.buffer);
-};
 
 module.exports = {
     multerUploads,
