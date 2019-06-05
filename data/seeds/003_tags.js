fa7af8cd854afc86d3765bdbcc75ce71425d51ab
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-06-05 17:19:35 -0400

    Creates seed file for tags table

---
+++ data/seeds/003_tags.js
@@ -0,0 +1,22 @@
+const faker = require("faker");
+const {genUniqueArr} = require("../helpers.js");
+
+const generateSeeds = () => {
+  const numOfTags = 20;
+  const tagsUnique = genUniqueArr(numOfTags, faker.random.word);
+  let arr = [];
+  for (let i = 0; i < numOfTags; i++) {
+    arr.push({
+      name: tagsUnique[i]
+    });
+  }
+  return arr;
+}
+
+exports.seed = async function(knex, Promise) {
+  const tags = await generateSeeds();
+  return knex.raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+    .then(function () {
+      return knex('tags').insert(tags);
+    });
+};
