c1e436582c067038fd8d7f87153de9cdbb0baec8
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-06-05 18:44:05 -0400

    Creates post_tags seed

---
+++ data/seeds/005_post_tags.js
@@ -0,0 +1,23 @@
+const faker = require("faker");
+
+const generateSeeds = () => {
+  const numOfReviews = 20;
+  let arr = [];
+  for (let i = 0; i < numOfReviews; i++) {
+    arr.push({
+      post_id: Math.ceil(Math.random() * 20),
+      tag_id: Math.ceil(Math.random() * 20)
+    });
+  }
+  return arr;
+}
+
+exports.seed = async function(knex, Promise) {
+  const post_tags = await generateSeeds();
+  
+  return knex
+    .raw("TRUNCATE TABLE post_tags RESTART IDENTITY CASCADE")
+    .then(function () {
+      return knex("post_tags").insert(post_tags);
+    });
+};
