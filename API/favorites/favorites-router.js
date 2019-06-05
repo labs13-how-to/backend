45cbb0f941ae1eb72fec01a7091b36b52e6e6436
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-06-05 18:14:24 -0500

    added get by post, get by favorite id, and delete endpoints

--- API/favorites/favorites-router.js
+++ API/favorites/favorites-router.js
@@ -5,8 +5,11 @@ const db = require('./favorites-model.js');
 module.exports = router;
 
 router.get('/favorites/users/:userId', getByUser);
-router.post('/posts/:postId/favorite', add);
-router.get('/favorites/users/:userId/posts/:postId', getId);
+router.get('/posts/:postId/favorites', getByPost);
+router.post('/posts/:postId/favorites', add);
+router.get('/favorites/users/:userId/posts/:postId', getIds);
+router.get('/favorites/:id', getFavoriteId);
+router.delete('/favorites/:id', remove);
 
 
 //functions
@@ -25,7 +28,37 @@ function getByUser(req, res) {
         })
 }
 
-function getId(req, res) {
+function getByPost(req, res) {
+    const id = req.params.postId;
+    db.getByPost(id)
+        .then(favorite => {
+            if (favorite) {
+                res.status(200).json(favorite)
+            } else {
+                res.status(404).json({ message: "The specified favorite does not exist." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err.message)
+        })
+}
+
+function getFavoriteId(req, res) {
+    const id = req.params.id;
+    db.getId(id)
+        .then(favorite => {
+            if (favorite) {
+                res.status(200).json(favorite)
+            } else {
+                res.status(404).json({ message: "The specified favorite does not exist." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err.message)
+        })
+}
+
+function getIds(req, res) {
     const { userId, postId } = req.params;
     db.getById(userId, postId)
         .then(favorite => {
@@ -50,4 +83,19 @@ function add(req, res) {
             return res.status(200).json(res)
         })
         .catch(err => { res.status(405).json({ err }) });
+}
+
+function remove(req, res) {
+    const { id } = req.params;
+    db.remove(id)
+        .then(favorite => {
+            if (favorite) {
+                res.status(200).json({ message: "The favorite has been successfully deleted." })
+            } else {
+                res.status(404).json({ message: "The specified favorite does not exist." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err.message)
+        });
 }
