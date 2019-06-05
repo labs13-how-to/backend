5edbe54425f948fb414507ee9a481aaf29637b06
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-06-05 17:19:05 -0400

    Moves genUniqueArr helper function into data/helpers.js

--- data/seeds/001_users.js
+++ data/seeds/001_users.js
@@ -1,15 +1,5 @@
 const faker = require("faker");
-
-const genUniqueArr = (num, fakerFn) => {
-  let arr = [];
-  while (arr.length < num) {
-    const temp = fakerFn();
-    if (!arr.includes(temp)) {
-      arr.push(temp);
-    }
-  }
-  return arr;
-};
+const {genUniqueArr} = require("../helpers.js");
 
 const generateSeeds = () => {
   const numOfUsers = 500;
