2055161b0af408eae07a9f1a3668515cd37b56a9
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-06-12 17:46:55 -0500

    Changed tags seed to static

--- data/seeds/004_tags.js
+++ data/seeds/004_tags.js
@@ -1,22 +1,69 @@
-const faker = require("faker");
-const {genUniqueArr} = require("../helpers.js");
 
-const generateSeeds = () => {
-  const numOfTags = 20;
-  const tagsUnique = genUniqueArr(numOfTags, faker.random.word);
-  let arr = [];
-  for (let i = 0; i < numOfTags; i++) {
-    arr.push({
-      name: tagsUnique[i]
-    });
-  }
-  return arr;
-}
-
-exports.seed = async function(knex, Promise) {
-  const tags = await generateSeeds();
+exports.seed = async function (knex, Promise) {
   return knex.raw("TRUNCATE TABLE tags RESTART IDENTITY CASCADE")
     .then(function () {
-      return knex('tags').insert(tags);
+      return knex('tags').insert([
+        {
+          id: 1,
+          name: 'Art',
+        },
+        {
+          id: 2,
+          name: 'Apparel',
+        },
+        {
+          id: 3,
+          name: 'Appliances',
+        },
+        {
+          id: 4,
+          name: 'Automotive',
+        },
+        {
+          id: 5,
+          name: 'Baby',
+        },
+        {
+          id: 6,
+          name: 'Beauty',
+        },
+        {
+          id: 7,
+          name: 'Cooking',
+        },
+        {
+          id: 8,
+          name: 'Crafts',
+        },
+        {
+          id: 9,
+          name: 'Electronics',
+        },
+        {
+          id: 10,
+          name: 'Furniture',
+        },
+        {
+          id: 11,
+          name: 'Gardening',
+        },
+        {
+          id: 12,
+          name: 'Home Improvement',
+        },
+        {
+          id: 13,
+          name: 'Outdoors',
+        },
+        {
+          id: 14,
+          name: 'Pets',
+        },
+        {
+          id: 15,
+          name: 'Toys',
+        },
+
+      ]);
     });
 };
