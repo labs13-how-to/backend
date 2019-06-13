b6a9e4ec2c8a4fe29e0a2ffe74e2e71084f2e7ee
mrsteveson <patrick.steveson727@gmail.com>
2019-06-13 15:07:04 -0500

    updating tag seeds

--- data/seeds/004_tags.js
+++ data/seeds/004_tags.js
@@ -1,22 +1,46 @@
-const faker = require("faker");
-const {genUniqueArr} = require("../helpers.js");
+// const faker = require("faker");
+// const {genUniqueArr} = require("../helpers.js");
 
-const generateSeeds = () => {
-  const numOfTags = 20;
-  const tagsUnique = genUniqueArr(numOfTags, faker.random.word);
-  let arr = [];
-  for (let i = 0; i < numOfTags; i++) {
-    arr.push({
-      name: tagsUnique[i]
-    });
-  }
-  return arr;
-}
+// const generateSeeds = () => {
+//   const numOfTags = 20;
+//   const tagsUnique = genUniqueArr(numOfTags, faker.random.word);
+//   let arr = [];
+//   for (let i = 0; i < numOfTags; i++) {
+//     arr.push({
+//       name: tagsUnique[i]
+//     });
+//   }
+//   return arr;
+// }
 
-exports.seed = async function(knex, Promise) {
-  const tags = await generateSeeds();
+// exports.seed = async function(knex, Promise) {
+//   const tags = await generateSeeds();
+//   return knex.raw("TRUNCATE TABLE tags RESTART IDENTITY CASCADE")
+//     .then(function () {
+//       return knex("tags").insert(tags);
+//     });
+// };
+
+exports.seed = function (knex, Promise) {
   return knex.raw("TRUNCATE TABLE tags RESTART IDENTITY CASCADE")
     .then(function () {
-      return knex('tags').insert(tags);
+      return knex("tags").insert([
+        {name: "Art"},
+        {name: "Apparel"},
+        {name: "Appliances"},
+        {name: "Automotive"},
+        {name: "Baby"},
+        {name: "Beauty"},
+        {name: "Cooking"},
+        {name: "Crafts"},
+        {name: "Electronics"},
+        {name: "Furniture"},
+        {name: "Gardening"},
+        {name: "Home Improvement"},
+        {name: "Outdoors"},
+        {name: "Pets"},
+        {name: "Toys"},
+      ]);
     });
 };
+
